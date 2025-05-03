5aa8d3d06a7551f41c42366bc9eb0d4b6c2e6af3
Meecheniy <115540051+Meecheniy@users.noreply.github.com>
2025-05-03 15:03:59 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const IMTPower = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Print("__ Калькулятор индекса массы тела __\n")
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
+}
